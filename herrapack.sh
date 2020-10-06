2a7b40558cbd9cd0dcc1c84648893c702fa48079
capitan comando <72467460+capitancomando@users.noreply.github.com>
2020-10-06 16:53:07 -0300

    Create herrapack.sh

---
+++ herrapack.sh
@@ -0,0 +1,118 @@
+#!bin/bash
+#by:Capitán Comando
+
+echo -e "\e[33m+ by:Capitán Comando +\e[0m"
+echo -e "\e[33m#######################\e[0m"
+echo ""
+sleep 3
+echo -e "\e[32m Iniciando instalacion de herramientas basicas termux\e[0m"
+sleep 1
+echo -e "\e[32m#######################\e[0m"
+echo -e "\e[32m+ buscando actualizaciones de paquetes  +\e[0m"
+echo -e "\e[32m#######################\e[0m"
+echo ""
+sleep 4
+
+echo -e "\e[32m Procesando... \e[0m"
+sleep 4
+clear
+echo "3"
+sleep 1
+clear
+echo "2"
+sleep 2
+clear
+echo "1"
+clear
+
+echo -e "\e[32m Instalando herramientas basicas de termux\e[0m"
+echo ""
+sleep 1
+
+mydir=$(apt update && apt upgrade -y)
+echo $apt update && apt upgrade -y
+
+echo -e "\e[32m Instalacion segura\e[0m"
+echo ""
+sleep 2
+
+echo -e "\e[32m Instalando paquetes automaticos\e[0m"
+echo ""
+sleep 1
+
+mydir=$(pkg install python2 -y)
+echo $ pkg install python2 -y
+
+mydir=$(pkg install python -y)
+echo $ pkg install python -y
+
+mydir=$(pkg install python3 -y)
+echo $ pkg install python3 -y
+
+mydir=$(pkg install curl -y)
+echo $ pkg install curl -y
+
+
+" ########################################### "
+echo NAHFER LEGION DE LEGIONES  3xploit
+" ############################################ "
+apto instalar figlet 
+figlet 3xploit
+apt install sl 
+sl
+echo 3xploit
+" #################### "
+figlet nmap
+" ################# "
+echo instalando nmap
+apto instalar nmap
+apto instalar python
+pitón figlet
+apto instalar php
+figlet php
+apto instalar python2
+" ################# "
+echo instalando python2
+" ################### "
+echo CAPITAN COMANDO
+figlet red_hawk
+clon de git https://github.com/Tuhinshubhra/RED_HAWK.git
+figlet credmap
+clon de git https://github.com/lightos/credmap.git
+figlet sqlmap
+clon de git https://github.com/sqlmapproject/sqlmap.git
+figlet recondog 
+clon de git https://github.com/UltimateHackers/ReconDog.git
+figlet ngrok 
+clon de git https://github.com/inconshreveable/ngrok.git
+apto instalar tor
+echo instalando tor
+apto instalar perl 
+apto instalar w3m 
+echo instalando w3m
+apto instalar apache2
+apto instalar cowsay
+cowsay 3xploit 
+figlet 3xploit
+" ########################## "
+echo Creado por: Capitan Comando
+" ########################## " 
+
+apt install util-linux coreutils vim python python2 ruby perl clang curl wget make openssl php zip unzip tar htop bison findutils git apr apr-util libtool pkg-config tmux termux-tools ncurses-utils ncurses postgresql termux-elf-cleaner openssl-tool -y
+
+echo -e "\e[32m########################\e[0m"
+
+echo -e "\e[32m+ TERMINANDO PROCESO +\e[0m"
+echo -e "\e[32m#######################\e[0m"
+echo ""
+sleep 1
+echo -e "\e[32m loading...\e[0m"
+sleep 1
+echo -e "\e[32m#######################\e[0m"
+echo -e "\e[32m+ HERRAMIENTAS TERMUX INSTALADAS  +\e[0m"
+echo -e "\e[32m#######################\e[0m"
+echo ""
+echo -e "\e[32m+ Creador: Capitan Comando +\e[0m"
+echo -e "\e[32m#######################\e[0m"
+echo -e "\e[32m+ Legion: NAHFER LEGION DE LEGIONES +\e[0m"
+echo -e "\e[32m#######################\e[0m"
